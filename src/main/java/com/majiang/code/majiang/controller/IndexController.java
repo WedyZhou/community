49c7466f87bc41aa83f1fa55594f08fd19533046
WedyZhou <1647534125@qq.com>
2020-03-12 19:05:42 +0800

    网站图标背景修改

--- src/main/java/com/majiang/code/majiang/controller/IndexController.java
+++ src/main/java/com/majiang/code/majiang/controller/IndexController.java
@@ -8,7 +8,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
     @GetMapping("/Index")
-    public  String hello( Model model){
+    public  String GetIndexHtml( Model model){
         return "index";
+    }
+    @GetMapping("/Contact")
+    public  String GetContactHtml( Model model){
+        return "Contact";
+    }
+    @GetMapping("/Guest")
+    public  String GetGuestHtml( Model model){
+        return "Guest";
+    }
+    @GetMapping("/HotelAndTraffic")
+    public  String GetHotelAndTrafficHtml( Model model){
+        return "HotelAndTraffic";
+    }
+    @GetMapping("/Organization")
+    public  String GetOrganizationHtml( Model model){
+        return "Organization";
+    }
+    @GetMapping("/Paper")
+    public  String GetPaperHtml( Model model){
+        return "Paper";
+    }
+    @GetMapping("/Plan")
+    public  String GetPlanHtml( Model model){
+        return "Plan";
+    }
+    @GetMapping("/Register")
+    public  String GetRegisterHtml( Model model){
+        return "Register";
+    }
+    @GetMapping("/Support")
+    public  String GetSupportHtml( Model model){
+        return "Support";
+    }
+    @GetMapping("/Wall_show")
+    public  String GetWall_showHtml( Model model){
+        return "Wall_show";
     }
 }
